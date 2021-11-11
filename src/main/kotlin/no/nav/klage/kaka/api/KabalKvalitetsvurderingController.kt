fd3635e26af4be2b798d681bf54b5f1a3e07f0f5
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-11-11 11:39:12 +0100

    Logs createKvalitetsvurdering call from kabal-api.
    Co-authored-by: Hågen Hasle <hagen.hasle@nav.no>
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- src/main/kotlin/no/nav/klage/kaka/api/KabalKvalitetsvurderingController.kt
+++ src/main/kotlin/no/nav/klage/kaka/api/KabalKvalitetsvurderingController.kt
@@ -6,6 +6,7 @@ import no.nav.klage.kaka.config.SecurityConfig.Companion.ISSUER_AAD
 import no.nav.klage.kaka.exceptions.MissingTilgangException
 import no.nav.klage.kaka.services.KvalitetsvurderingService
 import no.nav.klage.kaka.util.TokenUtil
+import no.nav.klage.kaka.util.getLogger
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.PostMapping
@@ -23,12 +24,18 @@ class KabalKvalitetsvurderingController(
     private val kabalApiName: String
 ) {
 
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = getLogger(javaClass.enclosingClass)
+    }
+
     @PostMapping
     fun createKvalitetsvurdering(): KabalKvalitetsvurderingView {
         val callingApplication = tokenUtil.getCallingApplication()
         if (callingApplication != kabalApiName) {
             throw MissingTilgangException("Wrong calling application: $callingApplication")
         }
+        logger.debug("New kvalitetsvurdering is requested by kabal-api")
         return KabalKvalitetsvurderingView(kvalitetsvurderingService.createKvalitetsvurdering().id)
     }
 }
