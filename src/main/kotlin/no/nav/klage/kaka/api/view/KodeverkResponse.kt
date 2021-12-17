ab6411077fe15c56edc88a6edf3e3e806908ef8d
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-12-17 15:22:05 +0100

    Removes unused parts of kodeverk response.

--- src/main/kotlin/no/nav/klage/kaka/api/view/KodeverkResponse.kt
+++ src/main/kotlin/no/nav/klage/kaka/api/view/KodeverkResponse.kt
@@ -1,20 +1,14 @@
 package no.nav.klage.kaka.api.view
 
 import no.nav.klage.kodeverk.*
-import no.nav.klage.kodeverk.hjemmel.Hjemmel
-import no.nav.klage.kodeverk.hjemmel.LovKilde
-import no.nav.klage.kodeverk.hjemmel.Registreringshjemmel
 import no.nav.klage.kodeverk.hjemmel.ytelseTilRegistreringshjemler
 
 data class KodeverkResponse(
-    val partIdTyper: List<KodeDto> = PartIdType.values().asList().toDto(),
     val sakstyper: List<KodeDto> = Type.values().asList().toDto(),
     val ytelser: List<YtelseKode> = getYtelser(),
     val utfall: List<KodeDto> = Utfall.values().asList().toDto(),
-    val registreringshjemler: List<KodeDto> = getRegistreringshjemler(),
     val enheter: List<KodeDto> = Enhet.values().asList().toDto(),
     val klageenheter: List<KlageenhetKode> = getKlageenheter(),
-    val lovKilder: List<KodeDto> = LovKilde.values().asList().toDto(),
 )
 
 fun getKlageenheter(): List<KlageenhetKode> =
@@ -33,19 +27,9 @@ fun getYtelser(): List<YtelseKode> =
             id = ytelse.id,
             navn = ytelse.navn,
             beskrivelse = ytelse.beskrivelse,
-            hjemler = ytelseToHjemler[ytelse] ?: emptyList(),
             lovKildeToRegistreringshjemler = ytelseToLovKildeToRegistreringshjemmel[ytelse] ?: emptyList(),
             enheter = ytelseTilVedtaksenheter[ytelse]?.map { it.toDto() } ?: emptyList(),
             klageenheter = ytelseTilKlageenheter[ytelse]?.map { it.toDto() } ?: emptyList(),
-        )
-    }
-
-fun getRegistreringshjemler(): List<KodeDto> =
-    Registreringshjemmel.values().map {
-        KodeDto(
-            id = it.id,
-            navn = it.lovKilde.beskrivelse + " - " + it.spesifikasjon,
-            beskrivelse = it.lovKilde.navn + " - " + it.spesifikasjon,
         )
     }
 
@@ -62,26 +46,10 @@ val ytelseToLovKildeToRegistreringshjemmel: Map<Ytelse, List<LovKildeToRegistrer
         }
     }
 
-val ytelseToHjemler: Map<Ytelse, List<KodeDto>> = mapOf(
-    Ytelse.OMS_OMP to
-            (Hjemmel.values().filter { it.kapittelOgParagraf != null && it.kapittelOgParagraf!!.kapittel == 9 }
-                    + Hjemmel.FTL + Hjemmel.MANGLER).toDto(),
-    Ytelse.OMS_OLP to
-            (Hjemmel.values().filter { it.kapittelOgParagraf != null && it.kapittelOgParagraf!!.kapittel == 9 }
-                    + Hjemmel.FTL + Hjemmel.MANGLER).toDto(),
-    Ytelse.OMS_PLS to
-            (Hjemmel.values().filter { it.kapittelOgParagraf != null && it.kapittelOgParagraf!!.kapittel == 9 }
-                    + Hjemmel.FTL + Hjemmel.MANGLER).toDto(),
-    Ytelse.OMS_PSB to
-            (Hjemmel.values().filter { it.kapittelOgParagraf != null && it.kapittelOgParagraf!!.kapittel == 9 }
-                    + Hjemmel.FTL + Hjemmel.MANGLER).toDto()
-)
-
 data class YtelseKode(
     val id: String,
     val navn: String,
     val beskrivelse: String,
-    val hjemler: List<KodeDto>,
     val lovKildeToRegistreringshjemler: List<LovKildeToRegistreringshjemler>,
     val enheter: List<KodeDto>,
     val klageenheter: List<KodeDto>,
