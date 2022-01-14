47f5b70df023e8288a889a5e4fe288f355294f16
Hågen Hasle <haagenhasle@gmail.com>
2022-01-14 15:04:30 +0100

    Lagt til ansattEnhet i userData

--- src/main/kotlin/no/nav/klage/kaka/api/view/UserData.kt
+++ src/main/kotlin/no/nav/klage/kaka/api/view/UserData.kt
@@ -1,6 +1,12 @@
 package no.nav.klage.kaka.api.view
 
-data class UserData(val ident: String, val navn: Navn, val klageenheter: List<KodeDto>, val roller: List<String>) {
+data class UserData(
+    val ident: String,
+    val navn: Navn,
+    val klageenheter: List<KodeDto>,
+    val ansattEnhet: KodeDto,
+    val roller: List<String>,
+) {
     data class Navn(
         val fornavn: String? = null,
         val etternavn: String? = null,
