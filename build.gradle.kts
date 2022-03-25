248158aebf695b9e180af2fa31d5be884e09cd3f
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-03-25 12:40:08 +0100

    Testing change in kodeverk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation("org.apache.poi:poi:$apachePoiVersion")
     implementation("org.apache.poi:poi-ooxml:$apachePoiVersion")
 
-    implementation("com.github.navikt:kabal-kodeverk:2022.03.21-12.30.b8382ea8d437")
+    implementation("com.github.navikt:kabal-kodeverk:aap-hjemler-SNAPSHOT")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:testcontainers:$testContainersVersion")
