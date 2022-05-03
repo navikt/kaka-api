90c5d898260ffe3d3818e9bc0aabb508cb62235b
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-05-03 17:00:36 +0200

    Updated kodeverk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation("org.apache.poi:poi:$apachePoiVersion")
     implementation("org.apache.poi:poi-ooxml:$apachePoiVersion")
 
-    implementation("com.github.navikt:kabal-kodeverk:2022.04.28-09.35.4a2b193f6e40")
+    implementation("com.github.navikt:kabal-kodeverk:2022.05.03-16.56.e17df3a31425")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:testcontainers:$testContainersVersion")
