b78055d8b372c65a1b9d1457c8d95a6d8d8b712f
Andreas Jonsson <andreas.jonsson@nav.no>
2022-08-31 16:04:37 +0200

    New swagger lib. Turned off security. Fixed config for test.
    Co-authored-by: Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>

--- src/main/kotlin/no/nav/klage/kaka/KakaApplication.kt
+++ src/main/kotlin/no/nav/klage/kaka/KakaApplication.kt
@@ -1,11 +1,9 @@
 package no.nav.klage.kaka
 
-import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableJwtTokenValidation(ignore = ["org.springdoc"])
 class KakaApplication
 
 fun main(args: Array<String>) {
