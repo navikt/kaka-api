227d7767933c06a7c9653196ecfdfeaee2216ac0
Andreas Jonsson <andreas.jonsson@nav.no>
2022-12-05 13:31:30 +0100

    Add support for enums when using reflection.
    Co-authored-by: Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>

--- src/main/kotlin/no/nav/klage/kaka/util/ReflectionUtil.kt
+++ src/main/kotlin/no/nav/klage/kaka/util/ReflectionUtil.kt
@@ -1,9 +1,30 @@
 package no.nav.klage.kaka.util
 
+import no.nav.klage.kaka.domain.kvalitetsvurdering.v1.KvalitetsvurderingV1
+import no.nav.klage.kaka.domain.kvalitetsvurdering.v2.KvalitetsvurderingV2
 import kotlin.reflect.KMutableProperty
 import kotlin.reflect.full.memberProperties
+import kotlin.reflect.javaType
 
+@OptIn(ExperimentalStdlibApi::class)
 fun setFieldOnObject(obj: Any, fieldToChange: Pair<String, Any?>) {
     val property = obj::class.memberProperties.firstOrNull { it.name == fieldToChange.first } as KMutableProperty<*>
-    property.setter.call(obj, fieldToChange.second)
+
+    when (property.returnType.javaType) {
+        KvalitetsvurderingV1.RadioValg::class.java -> {
+            property.setter.call(obj, KvalitetsvurderingV1.RadioValg.valueOf(fieldToChange.second.toString()))
+        }
+        KvalitetsvurderingV2.Radiovalg::class.java -> {
+            property.setter.call(obj, KvalitetsvurderingV2.Radiovalg.valueOf(fieldToChange.second.toString()))
+        }
+        KvalitetsvurderingV1.RadioValgRaadgivendeLege::class.java -> {
+            property.setter.call(obj, KvalitetsvurderingV1.RadioValgRaadgivendeLege.valueOf(fieldToChange.second.toString()))
+        }
+        KvalitetsvurderingV2.RadiovalgRaadgivendeLege::class.java -> {
+            property.setter.call(obj, KvalitetsvurderingV2.RadiovalgRaadgivendeLege.valueOf(fieldToChange.second.toString()))
+        }
+        else -> {
+            property.setter.call(obj, fieldToChange.second)
+        }
+    }
 }
