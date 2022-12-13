d82ba8f98d71d240dc446f8d22187bd07d9b8f53
Andreas Jonsson <andreas.jonsson@nav.no>
2022-12-13 14:10:13 +0100

    Add some indices for db optimization.
    Co-authored-by: Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>

---
+++ src/main/resources/db/migration/V1_15__Add_indices.sql
@@ -0,0 +1,3 @@
+CREATE INDEX saksdata_kvalitetsvurdering_version_idx ON kaka.saksdata (kvalitetsvurdering_version);
+CREATE INDEX saksdata_kvalitetsvurdering_id_idx ON kaka.saksdata (kvalitetsvurdering_id);
+CREATE INDEX registreringshjemmel_saksdata_id_idx ON kaka.registreringshjemmel (saksdata_id);
