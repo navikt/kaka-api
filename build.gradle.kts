6c3076771d514681f3e81029940df8d68bb4c5ff
oyvindwedoe <oyvind.norsted.wedoe@nav.no>
2023-02-21 12:46:00 +0100

    Updated kodeverk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("org.apache.poi:poi:$apachePoiVersion")
     implementation("org.apache.poi:poi-ooxml:$apachePoiVersion")
 
-    implementation("com.github.navikt:klage-kodeverk:v1.2.17")
+    implementation("com.github.navikt:klage-kodeverk:v1.2.18")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:testcontainers:$testContainersVersion")
