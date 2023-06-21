830566ee170153d294347b4d3ea135cbfb0bb75a
Øyvind Wedøe <oyvind.norsted.wedoe@nav.no>
2023-06-21 17:00:37 +0200

    Remove last invalid entries.

---
+++ src/main/resources/db/migration/V1_21__Remove_invalid_registreringshjemler.sql
@@ -0,0 +1,9 @@
+DELETE
+FROM kaka.registreringshjemmel
+WHERE id = '403'
+  AND saksdata_id = 'f3fb4131-b097-452c-83bc-d398f492e21d';
+
+DELETE
+FROM kaka.registreringshjemmel
+WHERE id = '403'
+  AND saksdata_id = 'd4dae109-650d-45e4-bd03-cd3898cfd2ec';
