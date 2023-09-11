778286c195b4e2aa27b22fb91951fbfb4f2697b7
Øyvind Wedøe <oyvind.norsted.wedoe@nav.no>
2023-09-11 11:57:20 +0200

    Adjust and remove invalid registreringshjemler.

---
+++ src/main/resources/db/migration/V1_22__Handle_invalid_registreringshjemler.sql
@@ -0,0 +1,50 @@
+UPDATE kaka.registreringshjemmel
+SET id = 'FTRL_14_7A'
+WHERE id = '403'
+  AND saksdata_id = '3a57738d-1c14-48c4-812b-5df7ce37779a';
+
+UPDATE kaka.registreringshjemmel
+SET id = 'FTRL_14_7A'
+WHERE id = '403'
+  AND saksdata_id = 'a4c19e44-70e4-4aa3-b143-b6bdf02f3b03';
+
+UPDATE kaka.registreringshjemmel
+SET id = 'FTRL_14_7A'
+WHERE id = '403'
+  AND saksdata_id = '8c50aaf1-af40-42a5-9fda-12a0a069f29c';
+
+UPDATE kaka.registreringshjemmel
+SET id = 'FTRL_14_7A'
+WHERE id = '403'
+  AND saksdata_id = '72e18797-af97-40de-920e-e2e85bfe811e';
+
+UPDATE kaka.registreringshjemmel
+SET id = 'FTRL_14_7A'
+WHERE id = '403'
+  AND saksdata_id = 'ac95bc1d-1616-40f1-a177-840556305d6c';
+
+--
+DELETE
+FROM kaka.registreringshjemmel
+WHERE id = '195'
+  AND saksdata_id = 'a4c19e44-70e4-4aa3-b143-b6bdf02f3b03';
+
+DELETE
+FROM kaka.registreringshjemmel
+WHERE id = '190'
+  AND saksdata_id = '8c50aaf1-af40-42a5-9fda-12a0a069f29c';
+
+DELETE
+FROM kaka.registreringshjemmel
+WHERE id = '204'
+  AND saksdata_id = '72e18797-af97-40de-920e-e2e85bfe811e';
+
+DELETE
+FROM kaka.registreringshjemmel
+WHERE id = '415'
+  AND saksdata_id = '72e18797-af97-40de-920e-e2e85bfe811e';
+
+DELETE
+FROM kaka.registreringshjemmel
+WHERE id = '190'
+  AND saksdata_id = 'ac95bc1d-1616-40f1-a177-840556305d6c';
