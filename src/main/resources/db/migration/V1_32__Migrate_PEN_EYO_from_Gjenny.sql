bc81ce07ec7855ce4cdec2b17584492bd037d1e1
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-03-13 12:44:21 +0100

    Migrate cases from Gjenny with wrong ytelse.

---
+++ src/main/resources/db/migration/V1_32__Migrate_PEN_EYO_from_Gjenny.sql
@@ -0,0 +1,8 @@
+UPDATE kaka.saksdata
+SET ytelse_id = '52'
+WHERE kvalitetsvurdering_id IN(
+                               'fbd550e8-a8ba-490c-a62a-b50c92bbb82e',
+                               '657aebc8-9faa-46c4-809d-0670c86f7be0',
+                               '13653496-2768-480c-bf52-7f8abb788e90'
+    )
+AND ytelse_id = '30';
