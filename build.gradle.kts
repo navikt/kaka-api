197f3f147ba5c03cc40d08faacf52108bfe814e4
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-04-02 09:47:46 +0200

    Updated kodeverk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ val apachePoiVersion = "5.4.0"
 val springDocVersion = "2.8.5"
 
 val shedlockVersion = "6.3.0"
-val klageKodeverkVersion = "1.9.27"
+val klageKodeverkVersion = "1.10.0"
 val ehcacheVersion = "3.10.8"
 val otelVersion = "1.48.0"
 
