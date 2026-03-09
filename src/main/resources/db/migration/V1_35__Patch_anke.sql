64a299387150b4cae6ade5e6688d32b1eb12ddfc
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-03-09 11:00:01 +0100

    Semicolon.

--- src/main/resources/db/migration/V1_35__Patch_anke.sql
+++ src/main/resources/db/migration/V1_35__Patch_anke.sql
@@ -1,3 +1,3 @@
 UPDATE kaka.saksdata
 SET utfall_id = '4'
-WHERE kvalitetsvurdering_id = 'e31997ca-1954-49e4-9c9d-46e7437896c7'
+WHERE kvalitetsvurdering_id = 'e31997ca-1954-49e4-9c9d-46e7437896c7';
