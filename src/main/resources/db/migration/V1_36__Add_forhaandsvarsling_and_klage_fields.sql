3d68eeef08ff50960e4f9c1811db02a3fdee2597
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-03-10 13:02:28 +0100

    Removed default values.

--- src/main/resources/db/migration/V1_36__Add_forhaandsvarsling_and_klage_fields.sql
+++ src/main/resources/db/migration/V1_36__Add_forhaandsvarsling_and_klage_fields.sql
@@ -1,14 +1,14 @@
 -- Add new fields for forhåndsvarsling (advance notice) to kvalitetsvurdering_v3
 ALTER TABLE kaka.kvalitetsvurdering_v3
-    ADD COLUMN sbr_brudd_paa_reglene_om_forhaandsvarsling BOOLEAN DEFAULT FALSE NOT NULL;
+    ADD COLUMN sbr_brudd_paa_reglene_om_forhaandsvarsling BOOLEAN;
 
 ALTER TABLE kaka.kvalitetsvurdering_v3
-    ADD COLUMN sbr_forhaandsvarsling_parten_ikke_varslet_foer_vedtak BOOLEAN DEFAULT FALSE NOT NULL;
+    ADD COLUMN sbr_forhaandsvarsling_parten_ikke_varslet_foer_vedtak BOOLEAN;
 
 ALTER TABLE kaka.kvalitetsvurdering_v3
-    ADD COLUMN sbr_forhaandsvarsling_parten_varslet_mangelfullt BOOLEAN DEFAULT FALSE NOT NULL;
+    ADD COLUMN sbr_forhaandsvarsling_parten_varslet_mangelfullt BOOLEAN;
 
 -- Add new field for klage rules not followed despite new decision
 ALTER TABLE kaka.kvalitetsvurdering_v3
-    ADD COLUMN sbr_brudd_paa_klage_ikke_fulgt_regler_fattet_nytt_enkeltvedtak BOOLEAN DEFAULT FALSE NOT NULL;
+    ADD COLUMN sbr_brudd_paa_klage_ikke_fulgt_regler_fattet_nytt_enkeltvedtak BOOLEAN;
 
