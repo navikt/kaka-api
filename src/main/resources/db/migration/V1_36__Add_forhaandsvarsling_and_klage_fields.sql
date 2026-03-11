24e45eafbebe37682246f34e5ac473ba671cd61b
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-03-11 11:15:33 +0100

    Temporary change to schema, to revert previous creation.

--- src/main/resources/db/migration/V1_36__Add_forhaandsvarsling_and_klage_fields.sql
+++ src/main/resources/db/migration/V1_36__Add_forhaandsvarsling_and_klage_fields.sql
@@ -1,14 +1,23 @@
--- Add new fields for forhåndsvarsling (advance notice) to kvalitetsvurdering_v3
-ALTER TABLE kaka.kvalitetsvurdering_v3
-    ADD COLUMN sbr_brudd_paa_reglene_om_forhaandsvarsling BOOLEAN;
+-- -- Add new fields for forhåndsvarsling (advance notice) to kvalitetsvurdering_v3
+-- ALTER TABLE kaka.kvalitetsvurdering_v3
+--     ADD COLUMN sbr_brudd_paa_reglene_om_forhaandsvarsling BOOLEAN DEFAULT false;
+--
+-- ALTER TABLE kaka.kvalitetsvurdering_v3
+--     ADD COLUMN sbr_forhaandsvarsling_parten_ikke_varslet_foer_vedtak BOOLEAN DEFAULT false;
+--
+-- ALTER TABLE kaka.kvalitetsvurdering_v3
+--     ADD COLUMN sbr_forhaandsvarsling_parten_varslet_mangelfullt BOOLEAN DEFAULT false;
+--
+-- -- Add new field for klage rules not followed despite new decision
+-- ALTER TABLE kaka.kvalitetsvurdering_v3
+--     ADD COLUMN sbr_brudd_paa_klage_ikke_fulgt_regler_fattet_nytt_enkeltvedtak BOOLEAN DEFAULT false;
+alter table kaka.kvalitetsvurdering_v3
+    drop column sbr_brudd_paa_reglene_om_forhaandsvarsling;
+alter table kaka.kvalitetsvurdering_v3
+    drop column sbr_forhaandsvarsling_parten_ikke_varslet_foer_vedtak;
+alter table kaka.kvalitetsvurdering_v3
+    drop column sbr_forhaandsvarsling_parten_varslet_mangelfullt;
+alter table kaka.kvalitetsvurdering_v3
+    drop column sbr_brudd_paa_klage_ikke_fulgt_regler_fattet_nytt_enkeltvedtak;
 
-ALTER TABLE kaka.kvalitetsvurdering_v3
-    ADD COLUMN sbr_forhaandsvarsling_parten_ikke_varslet_foer_vedtak BOOLEAN;
-
-ALTER TABLE kaka.kvalitetsvurdering_v3
-    ADD COLUMN sbr_forhaandsvarsling_parten_varslet_mangelfullt BOOLEAN;
-
--- Add new field for klage rules not followed despite new decision
-ALTER TABLE kaka.kvalitetsvurdering_v3
-    ADD COLUMN sbr_brudd_paa_klage_ikke_fulgt_regler_fattet_nytt_enkeltvedtak BOOLEAN;
 
