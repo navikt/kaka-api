88ec02bb2da94a6869f93ccf92773a4df6f75630
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-06-09 12:50:06 +0200

    FOR_SVA migrations.

---
+++ src/main/resources/db/migration/V1_40__FOR_migrations.sql
@@ -0,0 +1,55 @@
+UPDATE kaka.registreringshjemmel h
+SET id = 'FTRL_14_4A'
+FROM kaka.saksdata s
+WHERE h.saksdata_id = s.id
+  AND s.ytelse_id = '8'
+  AND h.id = 'FTRL_14_7A'
+  AND s.kvalitetsvurdering_version <> 1;
+
+UPDATE kaka.registreringshjemmel h
+SET id = 'FTRL_14_4B'
+FROM kaka.saksdata s
+WHERE h.saksdata_id = s.id
+  AND s.ytelse_id = '8'
+  AND h.id = 'FTRL_14_7B'
+  AND s.kvalitetsvurdering_version <> 1;
+
+UPDATE kaka.registreringshjemmel h
+SET id = 'FTRL_14_4C'
+FROM kaka.saksdata s
+WHERE h.saksdata_id = s.id
+  AND s.ytelse_id = '8'
+  AND h.id = 'FTRL_14_7C'
+  AND s.kvalitetsvurdering_version <> 1;
+
+UPDATE kaka.registreringshjemmel h
+SET id = 'FTRL_14_4D'
+FROM kaka.saksdata s
+WHERE h.saksdata_id = s.id
+  AND s.ytelse_id = '8'
+  AND h.id = 'FTRL_14_7D'
+  AND s.kvalitetsvurdering_version <> 1;
+
+UPDATE kaka.registreringshjemmel h
+SET id = 'FTRL_14_4E'
+FROM kaka.saksdata s
+WHERE h.saksdata_id = s.id
+  AND s.ytelse_id = '8'
+  AND h.id = 'FTRL_14_7E'
+  AND s.kvalitetsvurdering_version <> 1;
+
+UPDATE kaka.registreringshjemmel h
+SET id = 'FTRL_14_4F'
+FROM kaka.saksdata s
+WHERE h.saksdata_id = s.id
+  AND s.ytelse_id = '8'
+  AND h.id = 'FTRL_14_7F'
+  AND s.kvalitetsvurdering_version <> 1;
+
+UPDATE kaka.registreringshjemmel h
+SET id = 'FTRL_14_4G'
+FROM kaka.saksdata s
+WHERE h.saksdata_id = s.id
+  AND s.ytelse_id = '8'
+  AND h.id = 'FTRL_14_7I'
+  AND s.kvalitetsvurdering_version <> 1;
