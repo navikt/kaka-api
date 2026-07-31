bf60ad57036baf92a9f7d032107e1ac09c932f9b
Andreas Jonsson <andreas.jonsson@nav.no>
2026-07-31 09:49:44 +0200

    Migrate registreringshjemmel

---
+++ src/main/resources/db/migration/V1_41__DAG_migrations.sql
@@ -0,0 +1,51 @@
+UPDATE kaka.registreringshjemmel h
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.registreringshjemmel_kvalitetsvurdering_v2_vedtaket_brukt_feil_
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.registreringshjemmel_kvalitetsvurdering_v2_vedtaket_feil_konkre
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.registreringshjemmel_kvalitetsvurdering_v2_vedtaket_innholdet_i
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.registreringshjemmel_kvalitetsvurdering_v2_vedtaket_lovbestemme
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.r_k_v2_vedtaket_arheiv_hjemler_list
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.r_k_v2_vedtaket_bfh_hjemler_list
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.r_k_v3_saerregelverk_deltgff_hjemler_list
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.r_k_v3_saerregelverk_vbpfh_el_lt_hjemler_list
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.r_k_v3_saerregelverk_vbpfkr_hjemler_list
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.r_k_v3_sbr_begrunnelsesplikten_bnif_hjemler_list
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.r_k_v3_sbr_begrunnelsesplikten_bnih_hjemler_list
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
+
+UPDATE kaka.r_k_v3_sbr_begrunnelsesplikten_bvitr_hjemler_list
+SET id = '864'
+WHERE id = 'FTRL_4_16A';
