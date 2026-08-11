8afa6b0e876753d85034454d929a15ac80641cff
Andreas Jonsson <andreas.jonsson@nav.no>
2026-08-11 08:31:24 +0200

    Make requested changes for DAG.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ val mockkVersion = "1.14.11"
 val apachePoiVersion = "5.5.1"
 val springDocVersion = "3.0.3"
 val shedlockVersion = "7.7.0"
-val klageKodeverkVersion = "3.3.12"
+val klageKodeverkVersion = "3.3.15"
 val ehcacheVersion = "3.12.0"
 val otelVersion = "1.64.0"
 
---
+++ src/main/resources/db/migration/V1_42__more_DAG_migrations.sql
@@ -0,0 +1,7 @@
+UPDATE kaka.registreringshjemmel
+SET id = 'FS_DAG_1_6'
+WHERE id = 'FS_DAG_1_5';
+
+UPDATE kaka.registreringshjemmel_kvalitetsvurdering_v2_vedtaket_innholdet_i
+SET id = 'FS_DAG_1_6'
+WHERE id = 'FS_DAG_1_5';
