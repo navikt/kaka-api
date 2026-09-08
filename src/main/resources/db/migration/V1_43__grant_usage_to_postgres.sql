4ca02259d0611bbdae90a0a5952fc8041075cbba
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-09-08 20:52:35 +0200

    Adjusted flyway.

--- src/main/resources/db/migration/V1_43__grant_usage_to_postgres.sql
+++ src/main/resources/db/migration/V1_43__grant_usage_to_postgres.sql
@@ -6,6 +6,10 @@ $$
         THEN
             GRANT USAGE ON SCHEMA flyway_history_schema TO postgres;
             GRANT USAGE ON SCHEMA kaka TO postgres;
+            GRANT SELECT ON ALL TABLES IN SCHEMA flyway_history_schema TO postgres;
+            GRANT SELECT ON ALL TABLES IN SCHEMA kaka TO postgres;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA flyway_history_schema GRANT SELECT ON TABLES TO postgres;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA kaka GRANT SELECT ON TABLES TO postgres;
         END IF;
     END
 $$;
