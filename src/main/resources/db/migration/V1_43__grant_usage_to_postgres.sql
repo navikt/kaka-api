c6c9a21759799a5e55fb75620bce14c5be95285b
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-09-08 19:34:07 +0200

    Adjusted grant statement.

--- src/main/resources/db/migration/V1_43__grant_usage_to_postgres.sql
+++ src/main/resources/db/migration/V1_43__grant_usage_to_postgres.sql
@@ -1,2 +1,12 @@
-GRANT USAGE ON SCHEMA kaka TO postgres;
-GRANT USAGE ON SCHEMA flyway_history_schema TO postgres;
+DO
+$$
+    BEGIN
+        IF EXISTS
+            (SELECT 1 from pg_roles where rolname = 'postgres')
+        THEN
+            GRANT USAGE ON SCHEMA flyway_history_schema TO postgres;
+            GRANT USAGE ON SCHEMA kaka TO postgres;
+        END IF;
+    END
+$$;
+
